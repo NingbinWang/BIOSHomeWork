ec3b7f9f711b9901c55d0c977b9512aef3c62e33
alex1_wang <Alex1_Wang@asus.com>
2018-03-13 08:44:01 +0800

    创建event事件

--- Dxe/HomeWorkDxe.h
+++ Dxe/HomeWorkDxe.h
@@ -1,4 +1,4 @@
-///**********************************************************************
+//**********************************************************************
 //**********************************************************************
 //**                                                                  **
 //**        (C)Copyright 1985-2010, American Megatrends, Inc.         **
@@ -12,76 +12,40 @@
 //**********************************************************************
 //**********************************************************************
 
-#include <AmiDxeLib.h>
-#include <OEMLib.h>
-#include "..\HomeWork.h"
-#include <Efi.h>
+//**********************************************************************
+// $Header: /Alaska/Tools/template.h 6     1/13/10 2:13p Felixp $
+//
+// $Revision: 6 $
+//
+// $Date: 1/13/10 2:13p $
+//**********************************************************************
+// Revision History
+// ----------------
+// $Log: /Alaska/Tools/template.h $
+// 
+// 6     1/13/10 2:13p Felixp
+// 
+//**********************************************************************
+//<AMI_FHDR_START>
+//
+// Name:  <This File's Name>
+//
+// Description:	
+//
+//<AMI_FHDR_END>
+//**********************************************************************
+#ifndef __HOMEWORKDXE__H__
+#define __HOMEWORKDXE__H__
 
-VOID HomeWorkDxeHobnotify(
-        IN EFI_EVENT                    Event,
-        IN VOID                         *Context   
-)
-{
-    EFI_GUID                GuidHob = HOB_LIST_GUID;
-     HOMEWORK_HOB            *Homeworkhob=NULL;
-     EFI_GUID                 HomeworkHobGuid=HOMEWORK_HOB_GUID;
-     VOID                    *pHobList = NULL;
-     EFI_STATUS              Status;
-     pHobList = GetEfiConfigurationTable(pST, &GuidHob);
-     if (!pHobList) return;
-         
-     Homeworkhob = (HOMEWORK_HOB*)pHobList;
-     while (!EFI_ERROR(Status = FindNextHobByType(EFI_HOB_TYPE_GUID_EXTENSION, &Homeworkhob)))
-     {
-         if (guidcmp(&(*Homeworkhob).EfiHobGuidType.Name, &HomeworkHobGuid) == 0)
-                     break;
-     }
-     if (EFI_ERROR(Status)) 
-     {
-          return;
-     }else{
-              OEM_TRACE("data=%d\n",Homeworkhob->homeworkdata);
-     }
-     
-     pBS->SignalEvent(Event);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/****** DO NOT WRITE BELOW THIS LINE *******/
+#ifdef __cplusplus
 }
-
-EFI_STATUS HomeWorkDxeEntryPoint(
-	IN EFI_HANDLE ImageHandle,
-	IN EFI_SYSTEM_TABLE *SystemTable
-)
-{
-    
-    EFI_STATUS              Status;
-    EFI_EVENT              homeworkevent;
-    UINTN                    index=0;
-    
-    
-	InitAmiLib(ImageHandle,SystemTable);
-/*	
-	Status = gBS->CreateEvent(
-	         EVT_NOTIFY_SIGNAL,
-	         TPL_CALLBACK,
-	         HomeWorkDxeHobnotify,
-	        )
-*/	
-	
-	Status = pBS->CreateEvent(
-	             EVT_NOTIFY_WAIT,
-	             TPL_NOTIFY,
-	             (EFI_EVENT_NOTIFY)HomeWorkDxeHobnotify,
-	             (VOID*)NULL,
-	             &homeworkevent
-	            );
-	
-	Status =  pBS->WaitForEvent(1,&homeworkevent,&index);
-	
-	
-	
-	
-	return EFI_SUCCESS;
-}
-
+#endif
+#endif
 //**********************************************************************
 //**********************************************************************
 //**                                                                  **
