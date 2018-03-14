b02e4fac2684db1d7a5145057607cd21d75b4cd1
alex1_wang <Alex1_Wang@asus.com>
2018-03-14 09:12:00 +0800

    protocol的使用与创建

--- Dxe/HomeWorkDxe.c
+++ Dxe/HomeWorkDxe.c
@@ -15,36 +15,68 @@
 #include <AmiDxeLib.h>
 #include <OEMLib.h>
 #include "..\HomeWork.h"
-#include <Efi.h>
+#include <Uefi.h>
+#include "HomeWorkDxe.h"
 
-VOID HomeWorkDxeHobnotify(
-        IN EFI_EVENT                    Event,
-        IN VOID                         *Context   
+EFI_STATUS HomeWorkHobread(
+        IN  UINT8 device  
 )
 {
-    EFI_GUID                GuidHob = HOB_LIST_GUID;
-     HOMEWORK_HOB            *Homeworkhob=NULL;
-     EFI_GUID                 HomeworkHobGuid=HOMEWORK_HOB_GUID;
-     VOID                    *pHobList = NULL;
-     EFI_STATUS              Status;
-     pHobList = GetEfiConfigurationTable(pST, &GuidHob);
-     if (!pHobList) return;
-         
-     Homeworkhob = (HOMEWORK_HOB*)pHobList;
-     while (!EFI_ERROR(Status = FindNextHobByType(EFI_HOB_TYPE_GUID_EXTENSION, &Homeworkhob)))
-     {
-         if (guidcmp(&(*Homeworkhob).EfiHobGuidType.Name, &HomeworkHobGuid) == 0)
-                     break;
-     }
-     if (EFI_ERROR(Status)) 
-     {
-          return;
-     }else{
-              OEM_TRACE("data=%d\n",Homeworkhob->homeworkdata);
-     }
-     
-     pBS->SignalEvent(Event);
+       EFI_GUID                GuidHob = HOB_LIST_GUID;
+       HOMEWORK_HOB            *Homeworkhob=NULL;
+       EFI_GUID                 HomeworkHobGuid=HOMEWORK_HOB_GUID;
+       VOID                    *pHobList = NULL;
+       EFI_STATUS              Status;
+       pHobList = GetEfiConfigurationTable(pST, &GuidHob);
+       if (!pHobList) return EFI_UNSUPPORTED;
+           
+       Homeworkhob = (HOMEWORK_HOB*)pHobList;
+       while (!EFI_ERROR(Status = FindNextHobByType(EFI_HOB_TYPE_GUID_EXTENSION, &Homeworkhob)))
+       {
+           if (guidcmp(&(*Homeworkhob).EfiHobGuidType.Name, &HomeworkHobGuid) == 0)
+                       break;
+       }
+       if (!EFI_ERROR(Status)){
+                OEM_TRACE("device=%d,data=%d\n",device,Homeworkhob->homeworkdata);
+       }
+    return Status;
 }
+
+EFI_STATUS HomeWorkPciread(
+           IN  UINT8 busnum,
+           IN  UINT8 devicenum,
+           IN  UINT8 funcnum
+)
+{
+    OEM_TRACE("bus=%d,dev=%d,func=%d\n",busnum,devicenum,funcnum);
+    return EFI_SUCCESS;
+}
+
+EFI_HOMEWORKINTERFACE_PROTOCOL  HomeworkProtocol = {
+        HomeWorkHobread,
+        HomeWorkPciread,  
+};
+
+
+VOID HomeWorkDxeProtocolCallback(
+        IN EFI_EVENT                    Event,
+        IN VOID                       *Context
+)
+
+{
+    EFI_STATUS                             Status;
+    EFI_HOMEWORKINTERFACE_PROTOCOL         *HomeworkProtocolinterface;
+    Status = pBS->LocateProtocol(&gEfiHomeWorkProtocolGuid,
+                                 NULL,
+                                 &HomeworkProtocolinterface );
+    if (!EFI_ERROR(Status)) 
+    {
+        Status = HomeworkProtocolinterface->HomeWorkHobread(1); 
+        Status = HomeworkProtocolinterface->HomeWorkPciread(0,0,0);
+    }
+}
+
+
 
 EFI_STATUS HomeWorkDxeEntryPoint(
 	IN EFI_HANDLE ImageHandle,
@@ -54,31 +86,39 @@ EFI_STATUS HomeWorkDxeEntryPoint(
     
     EFI_STATUS              Status;
     EFI_EVENT              homeworkevent;
-    UINTN                    index=0;
-    
-    
+    VOID                    *Registration;
+
 	InitAmiLib(ImageHandle,SystemTable);
-/*	
-	Status = gBS->CreateEvent(
-	         EVT_NOTIFY_SIGNAL,
-	         TPL_CALLBACK,
-	         HomeWorkDxeHobnotify,
-	        )
-*/	
-	
-	Status = pBS->CreateEvent(
-	             EVT_NOTIFY_WAIT,
-	             TPL_NOTIFY,
-	             (EFI_EVENT_NOTIFY)HomeWorkDxeHobnotify,
-	             (VOID*)NULL,
-	             &homeworkevent
-	            );
-	
-	Status =  pBS->WaitForEvent(1,&homeworkevent,&index);
-	
-	
-	
-	
+	 Status = pBS->CreateEvent(
+	                     EVT_NOTIFY_SIGNAL,
+	                     TPL_CALLBACK,
+	                     HomeWorkDxeProtocolCallback,
+	                     (VOID*)NULL,
+	                     &homeworkevent
+	                    );
+     if (EFI_ERROR(Status)) 
+      {
+             return Status;
+      }
+	 Status = pBS->RegisterProtocolNotify (
+	                   &gEfiHomeWorkProtocolGuid,
+	                   homeworkevent,
+	                   &Registration
+	                   );
+	 if (EFI_ERROR(Status)) 
+	 {
+	         return Status;
+	 }
+	 Status = pBS->InstallProtocolInterface(
+	                          &ImageHandle,
+	                          &gEfiHomeWorkProtocolGuid,
+	                          EFI_NATIVE_INTERFACE,
+	                          &HomeworkProtocol
+	                   );
+	if (EFI_ERROR(Status)) 
+	{
+	     return Status;
+	}
 	return EFI_SUCCESS;
 }
 
--- Dxe/HomeWorkDxe.h
+++ Dxe/HomeWorkDxe.h
@@ -37,6 +37,33 @@
 #ifndef __HOMEWORKDXE__H__
 #define __HOMEWORKDXE__H__
 
+extern EFI_GUID gEfiHomeWorkProtocolGuid;
+typedef EFI_STATUS (* HOMEWORK_HOB_READ) (
+    IN  UINT8 device
+   
+);
+
+typedef EFI_STATUS (* HOMEWORK_PCI_READ) (
+    IN  UINT8 busnum,
+    IN  UINT8 devicenum,
+    IN  UINT8 funcnum
+);
+
+struct _EFI_HOMEWORK_PROTOCOL{
+    HOMEWORK_HOB_READ HomeWorkHobread;
+    HOMEWORK_PCI_READ HomeWorkPciread;
+};
+
+typedef struct _EFI_HOMEWORK_PROTOCOL  EFI_HOMEWORKINTERFACE_PROTOCOL;
+        // {47590bea-6178-498d-a905-3ce663c384d9} 
+#define EFI_HOMEWORKINTERFACE_PROTOCOL_GUID \
+          { \
+            0x47590bea, 0x6178, 0x498d, {0xa9, 0x5, 0x3c, 0xe6, 0x63, 0xc3, 0x84, 0xd9} \
+          }
+EFI_GUID gEfiHomeWorkProtocolGuid = EFI_HOMEWORKINTERFACE_PROTOCOL_GUID;
+#pragma pack()        
+        
+        
 #ifdef __cplusplus
 extern "C" {
 #endif
